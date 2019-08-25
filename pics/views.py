586688ccbaa162596a40dff33d7ac5ec8902c3d1
Pixel-0 <tiny0pixel@gmail.com>
2019-08-25 22:57:36 +0300

    add categories function to render categories template

--- pics/views.py
+++ pics/views.py
@@ -7,3 +7,9 @@ def welcome(request):
     categories = Category.objects.all()
     locations = Location.objects.all()
     return render(request, 'index.html',{"categories":categories,"locations":locations})
+
+def categories(request):
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    images = Image.objects.all()
+    return render(request, 'categories.html',{"categories":categories,"images":images,"locations":locations})
