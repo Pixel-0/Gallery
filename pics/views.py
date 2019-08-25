c544f0a35a96e0df801d57c6ff4fc783ae3111bc
Pixel-0 <tiny0pixel@gmail.com>
2019-08-25 19:11:54 +0300

    create views and define functions

--- pics/views.py
+++ pics/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
 
 # Create your views here.
+def welcome(request):
+    images = Image.objects.all()
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = 'Gartllery'
+    return render(request, 'index.html',{'images':images,'locations':locations,'categories':categories, 'title':title})
