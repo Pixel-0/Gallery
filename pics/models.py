94dfa6e20b25a318bee41573ab2a37237d39eab1
Pixel-0 <tiny0pixel@gmail.com>
2019-08-25 22:44:32 +0300

    create location model

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def save_location(self):
+        self.save()
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
