e657d7f77eac222ae6085ee8313e7424d9e29c1f
Pixel-0 <tiny0pixel@gmail.com>
2019-08-25 22:45:34 +0300

    add class category to models

--- pics/models.py
+++ pics/models.py
@@ -11,3 +11,20 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        category = cls.objects.filter(name__icontains=search_term)
+        return category
+
+    def __str__(self):
+        return self.name
