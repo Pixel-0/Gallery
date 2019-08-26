d75cf0b05bdf8598bdad56ae5d23fbedca1cc10a
Pixel-0 <tiny0pixel@gmail.com>
2019-08-26 12:09:05 +0300

    edit tests

--- pics/tests.py
+++ pics/tests.py
@@ -11,16 +11,16 @@ class ImageTestClass(TestCase):
                         upload_date ='2019-05-10 17:30:36.627472+03')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.james,Image))
+        self.assertTrue(isinstance(self.pixel,Image))
 
     def test_save_method(self):
-        self.james.save_image()
+        self.pixel.save_image()
         images = Image.objects.all()
         self.assertTrue(len(images) > 0)
 
     def test_delete_method(self):
-        self.james.save_image()
-        self.james.delete_image()
+        self.pixel.save_image()
+        self.pixel.delete_image()
         images = Image.objects.all()
         self.assertTrue(len(images) == 0)
 
